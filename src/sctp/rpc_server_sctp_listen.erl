2f2b78f3b8064fd0c913c3b4e5966282b4bae765
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-05-29 19:01:54 -0400

    fix: add release to rebar config and missing include

--- src/sctp/rpc_server_sctp_listen.erl
+++ src/sctp/rpc_server_sctp_listen.erl
@@ -7,6 +7,8 @@
 %% API
 -export([start_link/0]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-include("rpc_server.hrl").
  
 -record(state, {
     socket :: socket() | undefined,
